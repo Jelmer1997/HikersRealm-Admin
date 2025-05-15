4f4b231c5acfed432e068507a5e00005c62decc6
Jelmer <jelmer.wiegel.1997@gmail.com>
2025-05-15 08:54:08 +0200

    Initial commit with admin panel to create and edit trails and stages

---
+++ HikersRealmAdmin/Components/Models/Stage.cs
@@ -0,0 +1,19 @@
+﻿namespace HikersRealmAdmin.Components.Models
+{
+    public class Stage
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = "";
+        public int StageNumber { get; set; }
+        public string Start { get; set; } = "";
+        public string End { get; set; } = "";
+        public double Distance { get; set; }
+        public int Ascent { get; set; }
+        public int Descent { get; set; }
+        public double Time { get; set; }
+        public string Grade { get; set; } = "";
+        public string Status { get; set; } = "Inactive"; // <-- Default value here
+        public int TrailId { get; set; }
+    }
+
+}
---
+++ HikersRealmAdmin/Components/Models/Trail.cs
@@ -0,0 +1,19 @@
+﻿namespace HikersRealmAdmin.Components.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Difficulty { get; set; } = "Medium";
+        public int MinDays { get; set; }
+        public int MaxDays { get; set; }
+        public int Length { get; set; }
+        public string Status { get; set; } = "Inactive";
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string Gpx { get; set; } = string.Empty;
+        public List<Country> Countries { get; set; } = new();
+        public List<Stage> Stages { get; set; } = new();
+    }
+
+}
